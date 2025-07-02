4d81827c197017f4dc324dc05e7b2d8cfbeb2e59
fuguangshuai <78307513+fuguangshuai@users.noreply.github.com>
2025-07-02 02:03:21 +0000

    modified:   .env.development 	modified:   .env.production 	modified:   src/renderer/.env.development 	modified:   src/renderer/App.vue 	new file:   src/renderer/components/PasswordProtection.vue

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="app-container" :class="{ mobile: isMobile, noElectron: !isElectron }">
-    <n-config-provider :theme="theme === 'dark' ? darkTheme : lightTheme">
-      <n-dialog-provider>
-        <n-message-provider>
-          <router-view></router-view>
-          <traffic-warning-drawer v-if="!isElectron"></traffic-warning-drawer>
-        </n-message-provider>
-      </n-dialog-provider>
-    </n-config-provider>
+    <PasswordProtection>
+      <n-config-provider :theme="theme === 'dark' ? darkTheme : lightTheme">
+        <n-dialog-provider>
+          <n-message-provider>
+            <router-view></router-view>
+            <traffic-warning-drawer v-if="!isElectron"></traffic-warning-drawer>
+          </n-message-provider>
+        </n-dialog-provider>
+      </n-config-provider>
+    </PasswordProtection>
   </div>
 </template>
 
@@ -17,6 +19,7 @@ import { darkTheme, lightTheme } from 'naive-ui';
 import { computed, nextTick, onMounted, watch } from 'vue';
 import { useI18n } from 'vue-i18n';
 import { useRouter } from 'vue-router';
+import PasswordProtection from '@/components/PasswordProtection.vue';
 
 // import TrafficWarningDrawer from '@/components/TrafficWarningDrawer.vue';
 
